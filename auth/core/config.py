3b7d27606d06269f19457674b3daccd1f80950aa
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-13 12:51:20 +0700

    add database naming_convention

--- auth/core/config.py
+++ auth/core/config.py
@@ -38,6 +38,15 @@ class DataBaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class AccessTokenConfig(BaseModel):
     lifetime_seconds: int = 3600
     reset_password_token_secret: str
