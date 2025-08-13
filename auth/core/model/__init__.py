6b7ba81190c619e483a2931c11e07b9afb1cbbed
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-13 13:25:16 +0700

    create access token model

--- auth/core/model/__init__.py
+++ auth/core/model/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "helper_db",
     "Base",
     "User",
+    "AccessToken",
 )
 
 from .db_helper import helper_db
 from .base import Base
 from .user import User
+from .access_token import AccessToken
