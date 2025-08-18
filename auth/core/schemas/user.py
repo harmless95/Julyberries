769b30b8de3039f02e113a47f01a4a76e51a8c00
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-18 21:39:58 +0700

    update schema UserCreate and UserRead

--- auth/core/schemas/user.py
+++ auth/core/schemas/user.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional
+from typing import Optional, TYPE_CHECKING
 from pydantic import EmailStr, BaseModel, ConfigDict
 
-if TYPE_CHECKING:
-    from core.model import Role
+
+from .role import RoleRead
+from .role import RoleCreate
 
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
     name: str
-    role_id: Optional[int]
-    created_at: datetime
+    role: Optional["RoleCreate"]
 
     model_config = ConfigDict(from_attributes=True)
 
@@ -20,30 +20,7 @@ class UserRead(BaseModel):
     id: int
     email: EmailStr
     name: str
-    role_id: Optional["Role"]
+    role: Optional["RoleRead"]
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
-
-
-class UserUpdate(BaseModel):
-    email: EmailStr | None = None
-    password: str | None = None
-    name: str | None = None
-    role_id: Optional[int] | None = None
-    created_at: datetime | None = None
-
-
-class UserToken(BaseModel):
-    email: EmailStr
-    password: str
-
-    model_config = ConfigDict(from_attributes=True)
-
-
-class UserSchema(BaseModel):
-    model_config = ConfigDict(strict=True)
-
-    email: EmailStr
-    password: str
-    name: str
