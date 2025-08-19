a36febb5076cda89bc07c59029d2e7fc5d731c3e
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-19 08:01:40 +0700

    create login user, return access token and refresh token

--- auth/api/user_router.py
+++ auth/api/user_router.py
@@ -1,9 +1,12 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.CRUD.user_crud import create_user
+from api.CRUD.user_crud import create_user, auth_user
+from api.dependecies.helpers import create_access_token, create_refresh_token
 from core.model import helper_db, User
+from core.schemas.token import TokenBase
 from core.schemas.user import UserCreate, UserRead
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -23,3 +26,21 @@ async def register_user(
         data_user=data_user,
     )
     return UserRead.model_validate(user)
+
+
+@router.post(
+    "/login",
+    response_model=TokenBase,
+    status_code=status.HTTP_200_OK,
+)
+async def get_login(
+    session: Annotated[AsyncSession, Depends(helper_db.session_getter)],
+    data_user: OAuth2PasswordRequestForm = Depends(),
+):
+    user = await auth_user(session=session, data_user=data_user)
+    access_token = create_access_token(user=user)
+    refresh_token = create_refresh_token(user=user)
+    return TokenBase(
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
