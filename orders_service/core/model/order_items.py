8a1213ac0d94327fe6f3d7815da9acac89767574
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-20 21:40:08 +0700

    orders_service correct order_items model

--- orders_service/core/model/order_items.py
+++ orders_service/core/model/order_items.py
@@ -1,17 +1,23 @@
-import uuid
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import UUID, ForeignKey, DOUBLE_PRECISION
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, DOUBLE_PRECISION, UniqueConstraint
 
 from core.model import Base
 from core.model.mixins.id_int_primary_key import IdIntPrKey
 
+if TYPE_CHECKING:
+    from .orders import Order
+
 
 class OrderItem(Base, IdIntPrKey):
-    order_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), ForeignKey("orders.id")
+    __table_args__ = UniqueConstraint(
+        "order_id",
+        "product_id",
+        name="idx_uniq_order_product",
     )
-    product_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), ForeignKey("products.id")
-    )
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
     quantity: Mapped[int] = mapped_column()
     unit_price: Mapped[float] = mapped_column(DOUBLE_PRECISION)
+
+    order: Mapped["Order"] = relationship("Order", back_populates="product_item")
