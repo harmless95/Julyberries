6c7bf45f2df0c87aebd89e577ec7138f34dc68ea
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-20 11:43:28 +0700

    create get category by id

--- catalogs_services/api/CRUD/crud_category.py
+++ catalogs_services/api/CRUD/crud_category.py
@@ -2,6 +2,7 @@ from typing import Sequence
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from fastapi import HTTPException, status
 
 from core.model import Category
 
@@ -13,3 +14,18 @@ async def all_categories(
     result = await session.scalars(stmt)
     categories = result.all()
     return categories
+
+
+async def get_category(
+    session: AsyncSession,
+    category_id: int,
+) -> Category:
+    stmt = select(Category).where(Category.id == category_id)
+    result = await session.scalars(stmt)
+    category = result.first()
+    if not category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Invalid id: {category_id} not found",
+        )
+    return category
--- catalogs_services/api/routers/router_category.py
+++ catalogs_services/api/routers/router_category.py
@@ -2,8 +2,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.CRUD.crud_category import all_categories
-from core.model import helper_db
+from api.CRUD.crud_category import all_categories, get_category
+from core.model import helper_db, Category
 from core.schemas.schema_category import CategoryRead
 
 router = APIRouter(prefix="/categories", tags=["Category"])
@@ -19,3 +19,19 @@ async def get_all_categories(
 ) -> list[CategoryRead]:
     categories = await all_categories(session=session)
     return [CategoryRead.model_validate(category) for category in categories]
+
+
+@router.get(
+    "/{category_id}/",
+    response_model=CategoryRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_category_by_id(
+    session: Annotated[AsyncSession, Depends(helper_db.session_getter)],
+    category_id: int,
+) -> Category:
+    category = await get_category(
+        session=session,
+        category_id=category_id,
+    )
+    return category
