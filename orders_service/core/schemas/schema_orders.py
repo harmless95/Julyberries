b543ceb4ca1267f55bb6273599ca0d9c026b52e4
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-20 22:49:17 +0700

    orders_service create schema orders

---
+++ orders_service/core/schemas/schema_orders.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict
+
+
+class OrderCreate(BaseModel):
+    user_id: int
+    total_price: Decimal
+    cart_price: Decimal
+    delivery_price: Decimal
+    status: str
+
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
+
+
+class OrderRead(BaseModel):
+    id: int
+    user_id: int
+    total_price: Decimal
+    cart_price: Decimal
+    delivery_price: Decimal
+    status: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
