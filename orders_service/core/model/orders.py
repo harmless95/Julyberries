468b104eaa9588f9405ec7980bbf8babd557fc60
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-21 15:14:47 +0700

    orders_service add order items and Numeric

--- orders_service/core/model/orders.py
+++ orders_service/core/model/orders.py
@@ -1,20 +1,26 @@
+from typing import TYPE_CHECKING
 from datetime import datetime, timezone
 
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DECIMAL, TIMESTAMP, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DECIMAL, TIMESTAMP, Text, Numeric
 
-from core.model import Base
-from core.model.mixins.id_int_primary_key import IdIntPrKey
+from .base import Base
+from .mixins.id_int_primary_key import IdIntPrKey
+
+if TYPE_CHECKING:
+    from .order_items import OrderItem
 
 
 class Order(Base, IdIntPrKey):
     user_id: Mapped[int] = mapped_column()
-    total_price: Mapped[DECIMAL] = mapped_column()
-    cart_price: Mapped[DECIMAL] = mapped_column()
-    delivery_price: Mapped[DECIMAL] = mapped_column()
+    total_price: Mapped[DECIMAL] = mapped_column(Numeric(10, 2))
+    cart_price: Mapped[DECIMAL] = mapped_column(Numeric(10, 2))
+    delivery_price: Mapped[DECIMAL] = mapped_column(Numeric(10, 2))
     status: Mapped[str] = mapped_column(Text)
     created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         default=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
+
+    product_item: Mapped[list["OrderItem"]] = relationship(back_populates="order")
