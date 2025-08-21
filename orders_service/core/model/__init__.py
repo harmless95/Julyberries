09c196c58d548a250e05786d0812c48cbda864b7
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-21 15:15:44 +0700

    orders_service add views Base

--- orders_service/core/model/__init__.py
+++ orders_service/core/model/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "Order",
     "OrderItem",
     "helper_db",
+    "Base",
 )
 
 from .orders import Order
 from .order_items import OrderItem
 from .order_helper_db import helper_db
+from .base import Base
