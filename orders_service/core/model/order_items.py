6d51f6772aec3f51865d650d05906602e9775e1d
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-21 15:14:05 +0700

    orders_service fix format black

--- orders_service/core/model/order_items.py
+++ orders_service/core/model/order_items.py
@@ -2,21 +2,25 @@ from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, DOUBLE_PRECISION, UniqueConstraint
 
-from core.model import Base
-from core.model.mixins.id_int_primary_key import IdIntPrKey
+from .base import Base
+from .mixins.id_int_primary_key import IdIntPrKey
 
 if TYPE_CHECKING:
     from .orders import Order
 
 
 class OrderItem(Base, IdIntPrKey):
-    __table_args__ = UniqueConstraint(
+    # fmt: off
+    __table_args__ = (
+        UniqueConstraint(
         "order_id",
         "product_id",
         name="idx_uniq_order_product",
+    ),
     )
+    # fmt: on
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+    product_id: Mapped[int] = mapped_column()
     quantity: Mapped[int] = mapped_column()
     unit_price: Mapped[float] = mapped_column(DOUBLE_PRECISION)
 
