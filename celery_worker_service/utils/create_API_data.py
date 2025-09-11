1bd01c927b2f6360c7f1f6313116b0fb9cc0faad
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-09-11 10:38:38 +0700

    celery services created a currency dictionary from xml format

---
+++ celery_worker_service/utils/create_API_data.py
@@ -0,0 +1,39 @@
+import xmltodict
+import httpx
+import locale
+import asyncio
+
+locale.setlocale(locale.LC_NUMERIC, "ru_RU.UTF-8")
+
+
+def convert_number_locale(value):
+    try:
+        num = locale.atof(value)
+        if num.is_integer():
+            return int(num)
+        else:
+            return num
+    except:
+        return value
+
+
+BASE_URL = "https://www.cbr.ru/scripts/XML_daily.asp"
+
+
+async def create_data():
+    url = "https://www.cbr.ru/scripts/XML_daily.asp"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    response.encoding = "windows-1251"
+    dict_data = xmltodict.parse(response.text)
+    for valute in dict_data["ValCurs"]["Valute"]:
+        valute["NumCode"] = convert_number_locale(valute["NumCode"])
+        valute["Nominal"] = convert_number_locale(valute["Nominal"])
+        valute["Value"] = convert_number_locale(valute["Value"])
+        valute["VunitRate"] = convert_number_locale(valute["VunitRate"])
+    return dict_data
+
+
+if __name__ == "__main__":
+    data = asyncio.run(create_data())
+    print(data)
