d7c8a8ef568db263401829f667706d142c734056
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-09-13 14:57:48 +0700

    example api services correct config

--- example_api_services/core/config.py
+++ example_api_services/core/config.py
@@ -15,22 +15,6 @@ class Run(BaseModel):
     port: int = 8000
 
 
-class DataBaseConfig(BaseModel):
-    url: PostgresDsn
-    echo: bool = False
-    echo_pool: bool = False
-    pool_size: int = 50
-    max_overflow: int = 10
-
-    naming_convention: dict[str, str] = {
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-
-
 class LoggingConfig(BaseModel):
     log_level: Literal[
         "debag",
@@ -40,11 +24,6 @@ class LoggingConfig(BaseModel):
         "critical",
     ] = "info"
     log_format: str = LOG_DEFAULT_FORMAT
-
-
-class ConfigRedis(BaseModel):
-    url: str
-    expire_second: int = 3600
 
 
 class Setting(BaseSettings):
@@ -57,8 +36,6 @@ class Setting(BaseSettings):
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
     )
-    db: DataBaseConfig
-    redis_conf: ConfigRedis
     run: Run = Run()
     log: LoggingConfig = LoggingConfig()
 
