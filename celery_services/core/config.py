d411d6a145c7ed98b7257c13736db674b1db627b
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-09-14 00:40:47 +0700

    celery services create config

---
+++ celery_services/core/config.py
@@ -0,0 +1,54 @@
+from typing import Literal
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+
+# fmt: off
+LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+# fmt: on
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class ConfigValute(BaseModel):
+    url: str = "http://example_api_services:8000/valute/"
+    valute: str = "USD"
+
+
+class Run(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debag",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
+
+
+class ConfigRedis(BaseModel):
+    url: str
+    expire_second: int = 3600
+
+
+class Setting(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    redis_conf: ConfigRedis
+    run: Run = Run()
+    log: LoggingConfig = LoggingConfig()
+    valute_conf: ConfigValute = ConfigValute()
+
+
+setting = Setting()
