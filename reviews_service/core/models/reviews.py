b230b4d782f3db0a13aeee081beb5730b10ef5da
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-10-01 14:18:49 +0700

    reviews service create adding time and range rating

--- reviews_service/core/models/reviews.py
+++ reviews_service/core/models/reviews.py
@@ -1,15 +1,16 @@
 from uuid import UUID
 from beanie import Indexed, Document
-from pydantic import Field
-
-from core.config import setting
+from pydantic import Field, conint
+from datetime import datetime
 
 
 class Reviews(Document):
     product_id: Indexed(str)
-    user_id: UUID
-    rating: int
+    user_id: str
+    rating: conint(ge=1, le=5)
     text: str = Field(default="")
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
         name = "reviews-database"
