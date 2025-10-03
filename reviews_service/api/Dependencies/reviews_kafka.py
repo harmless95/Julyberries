c8930d847f16f073ecb2d4b88e5b35bd090efa01
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-10-03 12:24:54 +0700

    reviews service fix annotation

--- reviews_service/api/Dependencies/reviews_kafka.py
+++ reviews_service/api/Dependencies/reviews_kafka.py
@@ -1,9 +1,9 @@
 from aiokafka import AIOKafkaProducer
 
-from core.schema.reviews_schema import ReviewsRead
+from core.models.reviews import Reviews
 
 
-async def validate_price_decimal(review_data: ReviewsRead, producer: AIOKafkaProducer):
+async def validate_price_decimal(review_data: Reviews, producer: AIOKafkaProducer):
     message_json = review_data.model_dump_json()
     message_bytes = message_json.encode("utf-8")
     await producer.send_and_wait("REVIEW_CREATED", message_bytes)
