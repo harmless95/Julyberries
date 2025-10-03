4b829d514e195970c08a80f833a154fc8be097b7
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-10-03 12:23:46 +0700

    reviews service add annotation

--- reviews_service/api/routers/reviews.py
+++ reviews_service/api/routers/reviews.py
@@ -15,7 +15,7 @@ from api.Dependencies.kafka_state import get_producer
 
 from api.Dependencies.reviews_kafka import validate_price_decimal
 from core.config import setting
-from core.schema.reviews_schema import ReviewsCreate, ReviewsRead, ReviewsUpdate
+from core.schema.reviews_schema import ReviewsCreate, ReviewsUpdate
 
 from core.models.reviews import Reviews
 
@@ -24,13 +24,14 @@ router = APIRouter(prefix=setting.api.prefix, tags=[setting.api.tags])
 
 @router.post(
     "/",
+    response_model=Reviews,
     status_code=status.HTTP_201_CREATED,
 )
 async def add_reviews(
     data_reviews: ReviewsCreate,
     producer: Annotated[AIOKafkaProducer, Depends(get_producer)],
     request: Request,
-):
+) -> Reviews:
     result = await create_reviews(data_reviews=data_reviews, request=request)
     await validate_price_decimal(
         review_data=result,
@@ -39,19 +40,27 @@ async def add_reviews(
     return result
 
 
-@router.get("/{product_id}/")
+@router.get(
+    "/{product_id}/",
+    response_model=list[Reviews],
+    status_code=status.HTTP_200_OK,
+)
 async def get_reviews(
     product_id: UUID,
-):
+) -> list[Reviews]:
     result = await get_reviews_by_id(product_id=product_id)
     return result
 
 
-@router.patch("/{product_id}/")
+@router.patch(
+    "/{product_id}/",
+    response_model=Reviews,
+    status_code=status.HTTP_200_OK,
+)
 async def update_review(
     data_update: ReviewsUpdate,
     data_review: Reviews = Depends(get_review_by_id),
-):
+) -> Reviews:
     result = await update_review_by_product_id(
         data_update=data_update,
         data_review=data_review,
