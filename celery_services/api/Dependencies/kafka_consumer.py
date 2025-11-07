829818d65c84b09d714827e7b15dbbb17be40cab
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-11-07 16:24:40 +0700

    celery service kafka_consumer.py corrected connect server and add logging

--- celery_services/api/Dependencies/kafka_consumer.py
+++ celery_services/api/Dependencies/kafka_consumer.py
@@ -1,18 +1,21 @@
 import json
+import logging
 from aiokafka import AIOKafkaConsumer
+
+logger = logging.getLogger(__name__)
 
 
 async def consume():
     consumer = AIOKafkaConsumer(
         "ORDER_CREATED",
-        bootstrap_servers="localhost:9092",
-        group_id="my-group",
+        bootstrap_servers="kafka1:9092",
+        group_id="order-app",
         auto_offset_reset="earliest",
         value_deserializer=lambda m: json.loads(m.decode("utf-8")),
     )
     await consumer.start()
     try:
         async for message in consumer:
-            print("Received message:", message)
+            logger.warning("Received message: %s", message)
     finally:
         await consumer.stop()
