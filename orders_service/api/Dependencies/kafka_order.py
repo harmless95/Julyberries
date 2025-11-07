0b7a7ae81912487eb2b504bb27cf7f30fda617c2
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-11-07 16:27:35 +0700

    order service fix Order, OrderRead

--- orders_service/api/Dependencies/kafka_order.py
+++ orders_service/api/Dependencies/kafka_order.py
@@ -2,10 +2,12 @@ import json
 
 from aiokafka import AIOKafkaProducer
 
+from core.schemas.schema_orders import OrderRead
 from core.model import Order
 
 
 async def order_producer(data_order: Order, producer: AIOKafkaProducer, topic: str):
-    data_json = json.dumps(data_order)
+    data_read = OrderRead.model_validate(data_order)
+    data_json = json.dumps(data_read.json())
     data_encode = data_json.encode("utf-8")
     await producer.send_and_wait(topic, data_encode)
